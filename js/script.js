123e920959dfb926216841890f54edf4c42ec653
Mit-Patel <patelmit2012@gmail.com>
2020-10-08 19:40:01 +0530

    UI Changes

---
+++ js/script.js
@@ -0,0 +1,78 @@
+function createNav(activePage, fixed) {
+    let text = `
+        <nav class="navbar navbar-expand-md navbar-dark  bg-transparent mb-0 radius-0 ` + fixed + `" style="font-family:Courier;">
+            <a href="#" class="navbar-brand">The Cyber Alliance</a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <div class="navbar-nav ml-auto">
+                    <a href="index.html" class="nav-item nav-link ` + activePage[0] + ` px-3"><i class="fa fa-home"></i></i> Home</a>
+                    <a href="about.html" class="nav-item nav-link ` + activePage[1] + ` px-3 "> <i class="fa fa-info"></i> About TCA</a>
+                    <a href="events.html" class="nav-item nav-link ` + activePage[2] + ` px-3 "><i class="fa fa-calendar"></i> Events</a>
+                    <a href="achievements.html" class="nav-item nav-link ` + activePage[3] + ` px-3 "><i class="fa fa-trophy"></i> Achievements</a>
+                    <a href="code-of-conduct.html" class="nav-item nav-link ` + activePage[4] + ` px-3  "><i class="fa fa-terminal"></i> Code of Conduct</a>
+                    <a href="team-tca.html" class="nav-item nav-link ` + activePage[5] + ` px-3  "><i class="fa fa-users"></i> Team TCA</a>
+                </div>
+            </div>
+        </nav>
+    `;
+
+    if (activePage[0] === "active") {
+        $("#particles-js").append(text);
+    } else {
+        $("header").append(text);
+    }
+}
+
+function createFooter() {
+    $("footer").append(`
+        <div style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;margin: 20px 0;text-align: center;color: rgb(255, 255, 255, 0.8);">
+            In Association with Student Development Cell at <a href="https://rsu.ac.in/home/">Rashtriya Raksha University</a><br /> Managed by students of <a href="https://rsu.ac.in/school-of-information-technology-and-cyber-security/">SITAICS</a>
+        </div>
+    `);
+}
+
+function createEvent() {
+    let events = [{
+            topic: "Introducing The Cyber Alliance",
+            date: "12-10-2020",
+            time: "04:00 PM - 06:00 PM",
+            by: "The Cyber Alliance",
+            description: "A perfect kickstart for your learning adventure...",
+            image: "media/events/1.png"
+        },
+        {
+            topic: "Introducing The Cyber Alliance",
+            date: "12-10-2020",
+            time: "04:00 PM - 06:00 PM",
+            by: "The Cyber Alliance",
+            description: "A perfect kickstart for your learning adventure...",
+            image: "media/events/1.png"
+        },
+    ];
+
+    var text = "";
+
+    events.forEach(event => {
+        text += `
+            <li>
+                <div class="card">
+                    <div class="card-header text-center">
+                        Past Event
+                    </div>
+                    <div>                        
+                        <div style="background-image: url('` + event.image + `');background-size: cover;padding: 10%;">
+                            <h4 class="card-title">` + event.topic + `</h4>
+                            <h5 class="card-subtitle mb-2 text-white-50">` + event.by + `</h5>
+                            <p class="card-text">` + event.description + `</p>
+                            <a href="#" class="btn btn-primary disabled">Register Now</a>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        `;
+    });
+
+    $("#target>ul").append(text);
+}
